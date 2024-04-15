2b6cd05d41b1ee956578c7cc5f94bd9dc616aa86
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-04-15 14:04:05 +0000

    feat: Adding python setup tools to the application

---
+++ python-safetyRails.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import sys
+import safetyRails.safetyRails
+
+if __name__ == '__main__':
+    sys.exit(safetyRails.main.main())
---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+setup(
+    name = "safetyRails",
+    version = "0.1",
+    packages=["safetyRails"],
+    author="Aluisio Leonello Victal",
+    author_email = "alvictaç@gmail.com",
+    description = "train derailment detection through GPIO analysis",
+    license = "MIT",
+    keywords= "Rails, GPIO",
+    url = "",
+)
