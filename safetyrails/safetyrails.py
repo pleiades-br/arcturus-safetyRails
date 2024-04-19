63f268988e7226c0ca0ee58697464955596b23a8
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-04-19 20:41:29 +0000

    feat: Adding the code of decision maker

--- safetyrails/safetyrails.py
+++ safetyrails/safetyrails.py
@@ -1,5 +1,5 @@
-import espeak
-import dtmf
+from espeak import ESpeak
+from dtmf import DTMF
 import gpiod
 import subprocess
 import pyaudio
@@ -36,16 +36,7 @@ def play(file):
     stream.close()
     p.terminate()
 
-def main():
-    '''
-        Argument parsing with argparse and main job
-    '''
-    #GPIO pin to enable
-    gpio_pin_enable = 5
-
-    # GPIO pin to monitor
-    gpio_pin_monitor = 4
-
+def gpio_loop(gpio_pin_output, gpio_pin_monitor):
     # Request lines to configure and monitoring
     with gpiod.request_lines(
             path="/dev/gpiochip0",
@@ -58,7 +49,7 @@ def main():
                         bias=Bias.PULL_DOWN,
                         debounce_period=timedelta(seconds=0.5)
                 ),
-                gpio_pin_enable: 
+                gpio_pin_output: 
                     gpiod.LineSettings (
                         direction=Direction.OUTPUT,
                         output_value=Value.ACTIVE
@@ -68,7 +59,53 @@ def main():
             while True:
                 # Block until rising edge event happens
                 if line.read_edge_events():
-                     print("State change %d", line.get_value(gpio_pin_monitor))
+                     if (line.get_value(gpio_pin_monitor) == Value.INACTIVE):
+                        play("dtmf_pre.wav")
+                        play("text_output.wav")
+                        play("dtmf_pos.wav")
+
+
+
+def main():
+    '''
+        Argument parsing with argparse and main job
+    '''
+    parser = argparse.ArgumentParser(description='Prove of concept for Arcturus project')
+    parser.add_argument('dtmf_pre', type=str, help='Sequence of digits to generate preamble DTMF tones.')
+    parser.add_argument('text', type=str, help='String used to transform to audio')
+    parser.add_argument('dtmf_pos', type=str, help='Sequence of digits to generate preamble DTMF tones.')
+    parser.add_argument('--text-pitch', type=int, default=32, help=' Pitch adjustment, 0 to 99, default is 32')
+    parser.add_argument('--text-gap', type=int, default=-1, help=' Word gap. Pause between words, units of 10mS at the default speed')
+    parser.add_argument('--text-capitals', type=int, default=1, help='Indicate capital letters with: 1=sound, 2=the word "capitals"')
+    parser.add_argument('--text-speed', type=int, default=175, help=' Speed in approximate words per minute. The default is 175')
+    parser.add_argument('--text-volume', type=int, default=100, help='Volume, 0 to 200, default is 100')
+    parser.add_argument('--text-male-voice', action='store_true', help='Use a male voice instead a female voice')
+    parser.add_argument('--dtmf-duration', type=int, default=500, help='Duration of each tone in milliseconds (default: 500).')
+    parser.add_argument('--dtmf-silence-duration', type=int, default=25, help='Duration of silence between tones in milliseconds (default: 25).')
+    parser.add_argument('--dtmf-sample-rate', type=int, default=8000, help='Sample rate in Hz (default: 8000).')
+    args = parser.parse_args()
+
+    dtmf_o = DTMF(duration=args.dtmf_duration, silence_duration=args.dtmf_silence_duration,
+                    sample_rate=args.dtmf_sample_rate)
+    dtmf_o.generate_dtmf_tones(args.dtmf_pre)
+    dtmf_o.save_wave_file(filename="dtmf_pre.wav")
+
+    dtmf_o.generate_dtmf_tones(args.dtmf_pos)
+    dtmf_o.save_wave_file(filename="dtmf_pos.wav")
+
+    engine = ESpeak(volume=args.text_volume, gap=args.text_gap, capitals=args.text_capitals, 
+                    pitch=args.text_pitch, speed=args.text_speed, male_voice=args.text_male_voice)
+    
+    engine.save_wave_file(args.text,filename="text_output.wav")
+
+    #GPIO pin to enable
+    gpio_pin_enable = 5
+
+    # GPIO pin to monitor
+    gpio_pin_monitor = 6
+
+    gpio_loop(gpio_pin_enable, gpio_pin_monitor)
+
 
 if __name__ == '__main__':
     main()
