26a172cf76f09b7a8bd49288bc85d87b04c86dac
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-10-07 13:42:40 -0300

    fix: Fixing request line of gpio class

--- safetyrails/arcturus_gpios.py
+++ safetyrails/arcturus_gpios.py
@@ -9,31 +9,28 @@ class ArcturusGpios():
     GPIO_DEV_PATH="""/dev/gpiochip{n}"""
 
     def __init__(self, gpio_chip, gpio_id, consumer_name) -> None:
-        self.__line = None
         self.__gpio_id = gpio_id
-        print(self.GPIO_DEV_PATH.format(n=gpio_chip))
-        try:
-            with gpiod.request_lines(
-                path=str(self.GPIO_DEV_PATH.format(n=gpio_chip)),
-                consumer=consumer_name,
-                config={gpio_id: gpiod.LineSettings(direction=Direction.INPUT)},
-            ) as line:
-                self.__line = line
-        except Exception as error:
-            print(f'ArcturusGpio could not get {gpio_chip}:{gpio_id} for {consumer_name}\n \
-Error {type(error).__name__} - {error}')
+        self.__last_value = 0
+        self.__gpio_chip = self.GPIO_DEV_PATH.format(n=gpio_chip)
+        self.__consumer = consumer_name
 
     def get_value(self):
         """
             Return the current value (0 or 1) from a given gpio
-        Args:
-            gpio_id (int): the gpio_id 
 
         Returns:
             int: 0 or 1 for the current value
                 -1 for an error
         """
-        if self.__line is not None:
-            return self.__line.get_value(self.__gpio_id)
+        try:
+            with gpiod.request_lines(
+                path=self.__gpio_chip,
+                consumer=self.__consumer,
+                config={self.__gpio_id: gpiod.LineSettings(direction=Direction.INPUT)},
+            ) as line:
+                self.__last_value = line.get_value(self.__gpio_id)
+        except Exception as error:
+            print(f'ArcturusGpio could not get {gpio_chip}:{gpio_id} for {consumer_name}\n \
+Error {type(error).__name__} - {error}')
 
-        return -1
+        return self.__last_value
