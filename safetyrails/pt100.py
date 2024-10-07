2daf5d5559018e60d4d29dc754050f2312e5da22
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-10-07 08:27:27 -0300

    feat: Adding documentation and spliting the name paramenters in hw_name and name

--- safetyrails/pt100.py
+++ safetyrails/pt100.py
@@ -2,6 +2,18 @@ import os
 from shared_linux_const import LINUX_SYS_I2C_PATH
 
 class Pt100():
+    """
+    This class works with the data from ads122c04
+    Read the linux subsystem inside /sys and make all necessary
+    data transformation
+
+    Init args:
+        config: List if a dictionary of parameters {'name', 'ch1', 'ch2'}, 
+                each one containing a given name for the pads and the channel config
+
+    Raises:
+        FileNotFound: When could not find the ads122c04 inside /sys
+    """
     NAME="""AIN{mux1}_AVSS"""
     RAW_FILE="""in_voltage{mux1}_raw"""
     INPUT_FILE="""in_voltage{mux1}_input"""
@@ -9,11 +21,11 @@ class Pt100():
     MUX_RAW_FILE="""in_voltage{mux1}-voltage{mux2}_raw"""
     MUX_INPUT_FILE="""in_voltage{mux1}-voltage{mux2}_input"""
 
-    def __init__(self, config_list: list) -> None:
+    def __init__(self, config: list) -> None:
         self.__channels: list = []
         self.__dirpath: str = ""
         self.__get_dirpath()
-        self.__create_channel_list(config_list)
+        self.__create_channel_list(config)
 
     def __get_dirpath(self):
         for dirs in os.listdir(LINUX_SYS_I2C_PATH):
@@ -33,24 +45,29 @@ class Pt100():
         if self.__dirpath == "":
             raise FileNotFoundError("Could not find ti-ads122c04 sys folder")
 
-    def __create_channel_list(self, config_list: list):
-        for entry in config_list:
+    def __create_channel_list(self, config: list):
+        for entry in config:
             channel = {}
-            if isinstance(entry, list):
+            if 'name' not in entry or 'ch1' not in entry:
+                continue
+
+            if 'ch2' in entry:
                 channel = {
-                    "name": self.MUX_NAME.format(mux1=entry[0], mux2=entry[1]),
-                    "raw_file": self.MUX_RAW_FILE.format(mux1=entry[0], mux2=entry[1]),
+                    "name": entry['name'],
+                    "hw_name": self.MUX_NAME.format(mux1=entry['ch1'], mux2=entry['ch2']),
+                    "raw_file": self.MUX_RAW_FILE.format(mux1=entry['ch1'], mux2=entry['ch2']),
                     "raw_value": 0,
-                    "input_file": self.MUX_INPUT_FILE.format(mux1=entry[0], mux2=entry[1]),
+                    "input_file": self.MUX_INPUT_FILE.format(mux1=entry['ch1'], mux2=entry['ch2']),
                     "input_value": 0,
                     "offset": 0
                 }
             else:
                 channel = {
-                    "name": self.NAME.format(mux1=entry),
-                    "raw_file": self.RAW_FILE.format(mux1=entry),
+                    "name": entry['name'],
+                    "hw_name": self.NAME.format(mux1=entry['ch1']),
+                    "raw_file": self.RAW_FILE.format(mux1=entry['ch1']),
                     "raw_value": 0,
-                    "input_file": self.INPUT_FILE.format(mux1=entry),
+                    "input_file": self.INPUT_FILE.format(mux1=entry['ch1']),
                     "input_value": 0,
                     "offset": 0
                 }
@@ -70,5 +87,10 @@ class Pt100():
                 continue
 
     def get_sensor_data(self):
+        """ Get an update sensor data
+            
+        Returns:
+            dictionary: containing the raw and input data from the sensor.
+        """
         self.__update_data()
         return self.__channels
