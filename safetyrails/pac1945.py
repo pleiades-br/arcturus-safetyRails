51e1c838a686b68273a803d0a884409072f07638
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-10-07 08:30:52 -0300

    feat:  Adding documentation and spliting the name paramenters in hw_name and name for pac1945

--- safetyrails/pac1945.py
+++ safetyrails/pac1945.py
@@ -2,14 +2,27 @@ import os
 from shared_linux_const import LINUX_SYS_I2C_PATH
 
 class Pac1945():
+    """
+    This class works with the data from pac1945
+    Read the linux subsystem inside /sys and make all necessary
+    data transformation
+
+    Init args:
+        config: List if a dictionary of parameters {'name', 'ch1'}, 
+                each one containing a given name for the pads and the channel config
+
+    Raises:
+        FileNotFound: When could not find the ads122c04 inside /sys
+    """
+
     NAME="""AIN{mux1}"""
     RAW_FILE="""in_voltage{mux1}_raw"""
 
-    def __init__(self, config_list: list) -> None:
+    def __init__(self, config: list) -> None:
         self.__channels: list = []
         self.__dirpath: str = ""
         self.__get_dirpath()
-        self.__create_channel_list(config_list)
+        self.__create_channel_list(config)
 
     def __get_dirpath(self):
         for dirs in os.listdir(LINUX_SYS_I2C_PATH):
@@ -29,11 +42,14 @@ class Pac1945():
         if self.__dirpath == "":
             raise FileNotFoundError("Could not find pac1945_1 sys folder")
 
-    def __create_channel_list(self, config_list: list):
-        for entry in config_list:
+    def __create_channel_list(self, config: list):
+        for entry in config:
+            if 'name' not in entry or 'ch1' not in entry:
+                continue
             channel = {
-                    "name": self.NAME.format(mux1=entry),
-                    "raw_file": self.RAW_FILE.format(mux1=entry),
+                    "name": entry['name'],
+                    "hw_name": self.NAME.format(mux1=entry['ch1']),
+                    "raw_file": self.RAW_FILE.format(mux1=entry['ch1']),
                     "raw_value": 0,
                     "offset": 0
                 }
@@ -51,5 +67,10 @@ class Pac1945():
                 continue
 
     def get_sensor_data(self):
+        """ Get an update sensor data
+            
+        Returns:
+            dictionary: containing the raw and input data from the sensor.
+        """
         self.__update_data()
         return self.__channels
