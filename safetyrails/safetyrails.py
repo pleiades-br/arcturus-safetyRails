bd318ad014aac2d5b13452dbbeaa275e519016df
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-10-14 08:34:08 -0300

    feat: Adding more control to finish the threads

--- safetyrails/safetyrails.py
+++ safetyrails/safetyrails.py
@@ -1,6 +1,6 @@
 import os
 import threading
-from datetime import datetime, timedelta
+import time
 from hw_board import HWBoard
 from watchers import rail_gpio_watchdog, sensors_watchdog, ptas_gpio_watchog
 
@@ -13,14 +13,25 @@ def main():
         Argument parsing with argparse and main job
     '''
 
-    hwboard = HWBoard()
-    thread1 = threading.Thread(target=rail_gpio_watchdog, args=(hwboard, 5, False))
-    thread2 = threading.Thread(target=sensors_watchdog, args=(hwboard, 15, False))
-    thread3 = threading.Thread(target=ptas_gpio_watchog, args=(hwboard, 10, False))
+    try:
+        stop_threading = False
 
-    thread1.start()
-    thread2.start()
-    thread3.start()
+        hwboard = HWBoard()
+        thread1 = threading.Thread(target=rail_gpio_watchdog, args=(hwboard, 5, stop_threading))
+        thread2 = threading.Thread(target=sensors_watchdog, args=(hwboard, 15, stop_threading))
+        thread3 = threading.Thread(target=ptas_gpio_watchog, args=(hwboard, 10, stop_threading))
+        thread1.start()
+        thread2.start()
+        thread3.start()
+        while stop_threading is False:
+            # Sleep to avoid taking all cpu
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print(hwboard)
+        print("Interrupt by user!")
+    finally:
+        stop_threading = True
+
     thread1.join()
     thread2.join()
     thread3.join()
