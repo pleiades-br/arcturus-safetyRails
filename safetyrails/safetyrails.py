45bf1b6e80f66c7de5c9c089747b4a4f82a80e0f
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-10-19 16:55:36 -0300

    feat: Adding argument parsing when call the application

--- safetyrails/safetyrails.py
+++ safetyrails/safetyrails.py
@@ -1,17 +1,33 @@
 import os
 import threading
 import time
+import argparse
 from hw_board import HWBoard
 from watchers import rail_gpio_watchdog, sensors_watchdog, ptas_gpio_watchog
 from appconfig import SftrailsConfig
 
 
-def main():
+def argument_parser() -> str:
     '''
         Argument parsing with argparse and main job
     '''
+    parser = argparse.ArgumentParser(description='Rail sensors watchdog')
+    parser.add_argument(
+        '--config-file', 
+        type=str,
+        default='/etc/sftrails/sftrail.conf',
+        help='Path/filename to the config file'
+    )
+
+    args = parser.parse_args()
+    return args.config_file
+
+def main():
+    '''
+        Main application
+    '''
     stop_event = threading.Event()
-    appconf = SftrailsConfig()
+    appconf = SftrailsConfig(file_path=argument_parser())
     hwboard = HWBoard()
 
     thread1 = threading.Thread(target=rail_gpio_watchdog, args=(hwboard, appconf, stop_event))
